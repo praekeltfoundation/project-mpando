7de4dae1f9e9054770738097142307aa6a167416
Mitso Qalaba <mqalaba@gmail.com>
2020-01-15 12:03:48 +0200

    update db connection

--- api/app.js
+++ api/app.js
@@ -1,22 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
-var mongoose = require('mongoose');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+var index = require('./routes/index');
+const api = require('./routes/api/index');
 var app = express();
-
-
 mongoose.Promise = global.Promise;
-
 //Adds connection to database using mongoose
 //for <dbuser>:replace with your username, <dbpassword>: replace with your password.
 //<DATABASE_URL>: replace with database url, example:ds234562.mlab.com:17283
-mongoose.connect('mongodb://mitsoqalaba:nTB5Uf9EzH7auG!@ds263448.mlab.com:63448/appointments');
+mongoose.connect('mongodb://admin:C0mmunicationz@ds263448.mlab.com:63448/appointments',{
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => console.log('Database Connected!')).catch(err => {
+  console.log(`Database Connection Error: ${err.message}`);
+});
 
 //This enabled CORS, Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources (e.g. fonts)
 //on a web page to be requested from another domain outside the domain from which the first resource was served
@@ -32,34 +32,32 @@ app.all('/*', function(req, res, next) {
     next();
   }
 });
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/', index);
+app.use('/api', api);
+//app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
-
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
 module.exports = app;
