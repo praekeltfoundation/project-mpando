3d5711ac3fa91e187a0160300289f699b914ba1c
Mitso Qalaba <mqalaba@gmail.com>
2020-02-11 18:20:14 +0200

    update app ui:

--- mpando/src/components/articlesComp/Howto.js
+++ mpando/src/components/articlesComp/Howto.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './assets/Howto.css';
+import imageryOne from './assets/images/image-one.png';
 
 function Howto() {
   return (
@@ -8,13 +9,63 @@ function Howto() {
         <div className="Howto-guide__images">
           <img className="Howto-guide__thumbnail"
             alt='Popup'
-            src='./assets/images/image-one.png'
-            width='230px'
+            src={imageryOne}
+            width='430px'
             height='auto'
           />
         </div>
         <div className="Howto-guide__instructions">
-          <p>-</p>
+          <h3>Browser pop up</h3>
+          <p>When you visiting the webapp for the first time, the pop up install call to action is shown on the app.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Maecenas sed enim ut sem. Dolor sit amet consectetur adipiscing
+              elit duis tristique sollicitudin. Dui vivamus arcu felis bibendum.
+              Elit ut aliquam purus sit amet luctus. Ultricies leo integer malesuada nunc vel.
+              Felis bibendum ut tristique et egestas quis. Eu tincidunt tortor aliquam nulla
+              facilisi cras fermentum odio eu. Urna cursus eget nunc scelerisque.
+              Arcu non sodales neque sodales ut etiam. Tristique risus nec feugiat in fermentum.
+              Sed turpis tincidunt id aliquet risus. Ullamcorper morbi tincidunt ornare massa eget
+              egestas purus. Ipsum a arcu cursus vitae. </p>
+        </div>
+      </div>
+      <hr/>
+      <div className="Howto-guide__item">
+        <div className="Howto-guide__instructions">
+          <h3>Browser pop up</h3>
+          <p>When you visiting the webapp for the first time, the pop up install call to action is shown on the app.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Maecenas sed enim ut sem. Dolor sit amet consectetur adipiscing
+              elit duis tristique sollicitudin. Dui vivamus arcu felis bibendum.</p>
+        </div>
+        <div className="Howto-guide__images">
+          <img className="Howto-guide__thumbnail"
+            alt='Popup'
+            src={imageryOne}
+            width='430px'
+            height='auto'
+          />
+        </div>
+      </div>
+      <hr/>
+
+      <div className="Howto-guide__item">
+        <div className="Howto-guide__images">
+          <img className="Howto-guide__thumbnail"
+            alt='Popup'
+            src={imageryOne}
+            width='430px'
+            height='auto'
+          />
+        </div>
+        <div className="Howto-guide__instructions">
+          <h3>Browser pop up</h3>
+          <p>When you visiting the webapp for the first time, the pop up install call to action is shown on the app.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Maecenas sed enim ut sem. Dolor sit amet consectetur adipiscing
+              elit duis tristique sollicitudin. Dui vivamus arcu felis bibendum.</p>
         </div>
       </div>
     </div>
