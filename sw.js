d9b41bd74d99e66da12c6f87cb0fb7b88412408b
Mitso Qalaba <mqalaba@gmail.com>
2020-02-20 18:14:09 +0200

    Updates

--- sw.js
+++ sw.js
@@ -5,38 +5,6 @@ if ("function" === typeof importScripts) {
     console.log("Workbox is loaded");    // Disable logging
     workbox.setConfig({ debug: true });
 
-    // Add an event listener to detect when the registered
-    // service worker has installed but is waiting to activate
-    self.addEventListener("waiting", (event) => {
-      // `event.wasWaitingBeforeRegister` will be false if this is
-      // the first time the updated service worker is waiting.
-      // When `event.wasWaitingBeforeRegister` is true, a previously
-      // updated same service worker is still waiting.
-      // You may want to customize the UI prompt accordingly.
-      const prompt = createUIPrompt({
-        onAccept: async () => {
-          // Assuming the user accepted the update, set up a listener
-          // that will reload the page as soon as the previously waiting
-          // service worker has taken control.
-          self.addEventListener("controlling", (event) => {
-            window.location.reload();
-          });
-          // Send a message telling the service worker to skip waiting.
-          // This will trigger the `controlling` event handler above.
-          // Note: for this to work, you have to add a message
-          // listener in your service worker. See below.
-          self.addEventListener("message", (event) => {
-            if(event.data && event.data.type === "SKIP_WAITING"){
-              skipWaiting();
-            }
-          });
-        },
-        onReject: () => {
-          prompt.dismiss();
-        }
-      })
-    });
-
     //`generateSW` and `generateSWString` provide the option
     // to force update an exiting service worker.
     // Since we're using `injectManifest` to build SW,
@@ -58,30 +26,6 @@ if ("function" === typeof importScripts) {
   {
     "url": "static/css/index.css",
     "revision": "806e78ea570f8ccc782873964b17e0b6"
-  },
-  {
-    "url": "static/js/2.a0aa9681.chunk.js",
-    "revision": "073c9fe0ca6a90d2d1fb01d17bfdee6a"
-  },
-  {
-    "url": "static/js/2.a0aa9681.chunk.js.LICENSE",
-    "revision": "d7798c023baac53e2010f08a9edaab04"
-  },
-  {
-    "url": "static/js/main.a7f8d78b.chunk.js",
-    "revision": "2241c94cdf9783a8adbec8b439da793d"
-  },
-  {
-    "url": "static/js/main.a7f8d78b.chunk.js.map",
-    "revision": "23b73a82556a9c936542109968ead7d2"
-  },
-  {
-    "url": "static/js/runtime-main.5274d17f.js",
-    "revision": "500d76c9345a2cfaef351e5e6e14f0e9"
-  },
-  {
-    "url": "static/js/runtime-main.5274d17f.js.map",
-    "revision": "5a68cc398de81f4f930201fd29211f13"
   }
 ]);
 
