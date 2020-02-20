6d07c48a4ff609f013bdf3a5f9c6e6e51d2f42cd
Mitso Qalaba <mqalaba@gmail.com>
2020-02-20 18:30:30 +0200

    Updates

--- sw.js
+++ sw.js
@@ -5,6 +5,38 @@ if ("function" === typeof importScripts) {
     console.log("Workbox is loaded");    // Disable logging
     workbox.setConfig({ debug: true });
 
+    // Add an event listener to detect when the registered
+    // service worker has installed but is waiting to activate
+    self.addEventListener("waiting", (event) => {
+      // `event.wasWaitingBeforeRegister` will be false if this is
+      // the first time the updated service worker is waiting.
+      // When `event.wasWaitingBeforeRegister` is true, a previously
+      // updated same service worker is still waiting.
+      // You may want to customize the UI prompt accordingly.
+      const prompt = createUIPrompt({
+        onAccept: async () => {
+          // Assuming the user accepted the update, set up a listener
+          // that will reload the page as soon as the previously waiting
+          // service worker has taken control.
+          self.addEventListener("controlling", (event) => {
+            window.location.reload();
+          });
+          // Send a message telling the service worker to skip waiting.
+          // This will trigger the `controlling` event handler above.
+          // Note: for this to work, you have to add a message
+          // listener in your service worker. See below.
+          self.addEventListener("message", (event) => {
+            if(event.data && event.data.type === "SKIP_WAITING"){
+              skipWaiting();
+            }
+          });
+        },
+        onReject: () => {
+          prompt.dismiss();
+        }
+      })
+    });
+
     //`generateSW` and `generateSWString` provide the option
     // to force update an exiting service worker.
     // Since we're using `injectManifest` to build SW,
@@ -50,6 +82,7 @@ if ("function" === typeof importScripts) {
       })
     );
 
+    // Image caching
     workbox.routing.registerRoute(
       /\.(?:png|gif|jpg|jpeg|svg)$/,
       workbox.strategies.cacheFirst({
