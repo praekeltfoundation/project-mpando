9e485eba0d13a9ab45488874b84f495a848e7797
Mitso Qalaba <mqalaba@gmail.com>
2020-02-21 13:04:59 +0200

    Updates

---
+++ precache-manifest.f169daa91ab4c96d46edaeac8788698f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58fd5b2073d7d2076bee76cb910da2d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cebb65ea9c6484960d0c",
+    "url": "/static/css/main.f4771f71.chunk.css"
+  },
+  {
+    "revision": "3e70ef6ced60e8533094",
+    "url": "/static/js/2.3e46679b.chunk.js"
+  },
+  {
+    "revision": "d7798c023baac53e2010f08a9edaab04",
+    "url": "/static/js/2.3e46679b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cebb65ea9c6484960d0c",
+    "url": "/static/js/main.b63a4dda.chunk.js"
+  },
+  {
+    "revision": "caa0f2c14e18d2c77fa2",
+    "url": "/static/js/runtime-main.5274d17f.js"
+  },
+  {
+    "revision": "3d6a91a9113ea745f565e5d46ee7ea8f",
+    "url": "/static/media/image-one.3d6a91a9.png"
+  },
+  {
+    "revision": "baede6d4a9e73953df24cd6654b4d4c8",
+    "url": "/static/media/innovative-thoughts-1.baede6d4.jpg"
+  },
+  {
+    "revision": "b2da18c73b949c6742b772891e09dbb6",
+    "url": "/static/media/ndoh-logo.b2da18c7.png"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
 importScripts("/precache-manifest.f169daa91ab4c96d46edaeac8788698f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-// Global workbox
-console.log(workbox);
+  // Global workbox
   if (workbox) {
-    console.log("Workbox is loaded");    // Disable logging
+    // Disable logging
+    console.log("Workbox is loaded");
     workbox.setConfig({ debug: true });
 
     // Add an event listener to detect when the registered
     // service worker has installed but is waiting to activate
+    console.log(self);
     self.addEventListener("waiting", (event) => {
       // `event.wasWaitingBeforeRegister` will be false if this is
       // the first time the updated service worker is waiting.
@@ -38,56 +39,9 @@ console.log(workbox);
       })
     });
 
-    //`generateSW` and `generateSWString` provide the option
-    // to force update an exiting service worker.
-    // Since we're using `injectManifest` to build SW,
-    // manually overriding the skipWaiting();
-    self.addEventListener("install", event => {
-      self.skipWaiting();
-      window.location.reload();
-    });
 
-    workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
-    workbox.routing.registerRoute(
-      /\.css$/,
-      workbox.strategies.staleWhileRevalidate({
-        cacheName: 'css-cache'
-      })
-    );
 
-    workbox.routing.registerRoute(
-      /\.js$/,
-      workbox.strategies.staleWhileRevalidate({
-        cacheName: 'js-cache'
-      })
-    );
-
-    // Image caching
-    workbox.routing.registerRoute(
-      /\.(?:png|gif|jpg|jpeg|svg)$/,
-      workbox.strategies.cacheFirst({
-        cacheName: "images-cache",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 20,
-            maxAgeSeconds: 7 * 24 * 60 * 60 // Weekly cache
-          })
-        ]
-      })
-    );
-
-    workbox.routing.registerRoute(
-      new RegExp("https://fonts.(?:.googlepis|gstatic).com/(.*)"),
-      workbox.strategies.cacheFirst({
-        cacheName: "googleapis",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 3
-          })
-        ]
-      })
-    );
-
+    workbox.precaching.precacheAndRoute(self.__precacheManifest);
   } else {
     console.error("Workbox could not be loaded. No offline support");
   }
