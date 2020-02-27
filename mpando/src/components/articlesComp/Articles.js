99dde1c54572075f94e84096979d2629ba3cad2c
Mitso Qalaba <mqalaba@gmail.com>
2020-02-27 15:13:25 +0200

    cleanup stories page bug

--- mpando/src/components/articlesComp/Articles.js
+++ mpando/src/components/articlesComp/Articles.js
@@ -23,12 +23,13 @@ class Aricles extends Component {
   render() {
     const { data } = this.state;
     return (
-      <div className="Articles">
+      <div className="Articles" key='a'>
         {data.map((item, index) =>
-          <Fragment>
-            <div className="Articles-media" key={item.media.iframe.title}>
-              <div className="Articles-media__source">
+          <Fragment key='b'>
+            <div className="Articles-media" key='c'>
+              <div className="Articles-media__source" key='d'>
                 <iframe
+                  key={item.media.iframe.title}
                   title={item.media.iframe.title}
                   width={item.media.iframe.width}
                   height={item.media.iframe.height}
@@ -38,24 +39,25 @@ class Aricles extends Component {
               </div>
             </div>
 
-            <div className="Articles-info">
-              <div className="Articles-info-wrapper">
-                <h3 className="Articles-info__title">{item.info.title}</h3>
+            <div className="Articles-info" key='e'>
+              <div className="Articles-info-wrapper" key='f'>
+                <h3 className="Articles-info__title" key={item.info.title}>{item.info.title}</h3>
               </div>
-              <div className="Articles__list">
+              <div className="Articles__list" key='g'>
                 {item.info.articleItems.map((post,i) =>
                   <div className="Articles__item" key={i}>
-                    <div className="Articles__image">
+                    <div className="Articles__image" key='h'>
                       <img className="Articles__thumbnail"
+                        key={post.image.alt}
                         alt={post.image.alt}
                         src={post.image.src}/>
                     </div>
-                    <div className="Articles__excerpt">
-                      <div className="Articles__heading">
-                        <h4 className="Articles__title">{post.title}</h4>
+                    <div className="Articles__excerpt" key='i'>
+                      <div className="Articles__heading" key='j'>
+                        <h4 className="Articles__title" key={post.title}>{post.title}</h4>
                       </div>
-                      <div className="Articles__text">
-                        <p>{post.description}</p>
+                      <div className="Articles__text" key='k'>
+                        <p key='l'>{post.description}</p>
                       </div>
                     </div>
                 </div>
