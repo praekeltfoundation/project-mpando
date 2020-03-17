482df1b3022b2ce703f996b4c0612551ef711920
Mitso Qalaba <mqalaba@gmail.com>
2020-03-17 20:50:59 +0200

    Updates

--- sw.js
+++ sw.js
@@ -6,14 +6,17 @@ const SW_VERSION = 'v1';
 const CACHE_NAME = 'mpando_sw';
 let OFFLINE_CACHE = [
   '/',
-  `${process.env.PUBLIC_URL}/offline.html`,
-  `${process.env.PUBLIC_URL}/static/images/logo.png`,
-  `${process.env.PUBLIC_URL}/static/css/index.css`,
-  `${process.env.PUBLIC_URL}/static/images/theme-spindle/ms-icon-144x144.png`,
-  `${process.env.PUBLIC_URL}/static/images/theme-spindle/apple-icon-72x72.png`,
-  `${process.env.PUBLIC_URL}/static/images/theme-spindle/apple-icon-152x152.png`,
-  `${process.env.PUBLIC_URL}/static/images/theme-spindle/apple-icon-152x152.png`,
+  // `${process.env.PUBLIC_URL}/offline.html`,
+  // `${process.env.PUBLIC_URL}/static/images/logo.png`,
+  // `${process.env.PUBLIC_URL}/static/css/index.css`,
+  // `${process.env.PUBLIC_URL}/static/images/theme-spindle/ms-icon-144x144.png`,
+  // `${process.env.PUBLIC_URL}/static/images/theme-spindle/apple-icon-72x72.png`,
+  // `${process.env.PUBLIC_URL}/static/images/theme-spindle/apple-icon-152x152.png`,
+  // `${process.env.PUBLIC_URL}/static/images/theme-spindle/apple-icon-152x152.png`,
 ];
+
+
+console.log(`${process.env.PUBLIC_URL}`);
 
 if (workbox) {
   self.addEventListener("message", event => {
