abdc7a8cc7d57ca8439345a6b9500a2e3c2563d2
Mitso Qalaba <mqalaba@gmail.com>
2020-03-17 14:34:17 +0200

    cleanup sw

--- mpando/src/serviceWorker.js
+++ mpando/src/serviceWorker.js
@@ -1,10 +1,9 @@
 import { Workbox } from "workbox-window";
 
-const CACHE = 'sw-cache';
-const VERSION = 'v1';
+const SW_VERSION = '1';
+const CACHE_NAME = 'mpando_sw';
 
 export function register(config) {
-  //process.env.NODE_ENV === 'production' &&
   if ('serviceWorker' in navigator) {
     /*
       !IMPORTANT
@@ -15,14 +14,13 @@ export function register(config) {
       console.log('Cross Domain Restriction');
       return;
     }
-
     /*
       SW - WINDOW LOAD
     */
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
       const wb = new Workbox(swUrl);
-
+      console.log('zeWB serviceWorker:',wb);
       // FETCHING OF SW
       fetch(swUrl, {
         headers: { 'Service-Worker': 'script'}
@@ -43,22 +41,53 @@ export function register(config) {
               Fires when the registered SERVICE WORKER is
               Installed but is WAITING to Activate.
           */
-          wb.addEventListener("waiting", event => {
-              updateButton.className = "show";
-              updateButton.addEventListener("click", () => {
-                // Set up a listener that will reload the page as
-                // soon as the previously waiting service worker has taken control.
-                wb.addEventListener("controlling", event => {
-                  window.location.reload();
-                });
-                // Send a message telling the service worker to skip waiting.
-                // This will trigger the `CONTROLLING` event handler above.
-                wb.messageSW({ type: "SKIP_WAITING" });
-              });
+
+          // https://developers.google.com/web/tools/workbox/modules/workbox-window#example-first-active
+          wb.addEventListener('activated', (event) => {
+            // `event.isUpdate` will be true if another version of the service
+            // worker was controlling the page when this version was registered.
+            if (!event.isUpdate) {
+              console.log('Service worker activated for the first time!');
+              // If your service worker is configured to precache assets, those
+              // assets should all be available now?
+            }
           });
 
-          // REGISTER SW ||
+
+          // https://developers.google.com/web/tools/workbox/modules/workbox-window#example-waiting
+          wb.addEventListener("waiting", event => {
+            updateButton.className = "show";
+            updateButton.addEventListener("click", () => {
+              // Set up a listener that will reload the page as
+              // soon as the previously waiting service worker has taken control.
+              wb.addEventListener("controlling", event => {
+                window.location.reload();
+              });
+              // Send a message telling the service worker to skip waiting.
+              // This will trigger the `CONTROLLING` event handler above.
+              wb.messageSW({ type: "SKIP_WAITING" });
+            });
+          });
+
+          // https://developers.google.com/web/tools/workbox/modules/workbox-window#example-broadcast-updates
+          // wb.addEventListener("message", event => {
+          //   if (event.data && event.data.type === "SKIP_WAITING") {
+          //     skipWaiting();
+          //   }
+          // });
+          wb.addEventListener('message', (event) => {
+            if (event.data.type === 'CACHE_UPDATED') {
+              const {updatedURL} = event.data.payload;
+              console.log(`A newer version of ${updatedURL} is available!`);
+            }
+          });
+
+
+
+          // REGISTER SW
           wb.register();
+          //const swVersion = await wb.messageSW({type: 'GET_VERSION'});
+          //console.log('Service Worker version:', swVersion);
         }
       }).catch((err) => {
         console.log(`No internet connection found. App is running in offline mode:: ${err}`);
--- mpando/src/sw-custom.js
+++ mpando/src/sw-custom.js
@@ -1,72 +1,51 @@
-  // Global workbox
-  if (workbox) {
-    // Disable logging
-    console.log("Workbox is loaded");
+// Global workbox
+if (workbox) {
+  console.log('zeWB sw-custom::',workbox);
 
 
-    addEventListener("message", event => {
-      if (event.data && event.data.type === "SKIP_WAITING") {
-        skipWaiting();
-      }
-    });
+  workbox.setConfig({ debug: true });
+  workbox.core.setCacheNameDetails({
+    prefix: "mpando",
+    suffix: "v1"
+  });
 
+  workbox.routing.registerRoute(
+    /\.css$/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'css-cache'
+    })
+  );
+  workbox.routing.registerRoute(
+    /\.js$/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'js-cache'
+    })
+  );
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.networkFirst({
+      cacheName: "images-cache",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 5,
+          maxAgeSeconds: 1 * 24 * 60 * 60 // Weekly cache
+        })
+      ]
+    })
+  );
+  workbox.routing.registerRoute(
+    new RegExp("https://fonts.(?:.googlepis|gstatic).com/(.*)"),
+    workbox.strategies.cacheFirst({
+      cacheName: "googleapis",
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 3
+        })
+      ]
+    })
+  );
 
-    //workbox.skipWaiting();
-    /*
-    By default, a page's fetches won't go through a service worker
-    unless the page request itself went through a service worker.
-    So you'll need to refresh the page to see the
-    effects of the service worker.
-
-    clients.claim() can override this default, and take control of non-controlled pages.
-    */
-    //workbox.clientsClaim();
-    workbox.setConfig({ debug: true });
-    workbox.core.setCacheNameDetails({
-      prefix: "mpando",
-      suffix: "v1"
-    });
-
-    workbox.routing.registerRoute(
-      /\.css$/,
-      workbox.strategies.staleWhileRevalidate({
-        cacheName: 'css-cache'
-      })
-    );
-
-    workbox.routing.registerRoute(
-      /\.js$/,
-      workbox.strategies.staleWhileRevalidate({
-        cacheName: 'js-cache'
-      })
-    );
-
-    workbox.routing.registerRoute(
-      /\.(?:png|gif|jpg|jpeg|svg)$/,
-      workbox.strategies.networkFirst({
-        cacheName: "images-cache",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 5,
-            maxAgeSeconds: 1 * 24 * 60 * 60 // Weekly cache
-          })
-        ]
-      })
-    );
-
-    workbox.routing.registerRoute(
-      new RegExp("https://fonts.(?:.googlepis|gstatic).com/(.*)"),
-      workbox.strategies.cacheFirst({
-        cacheName: "googleapis",
-        plugins: [
-          new workbox.expiration.Plugin({
-            maxEntries: 3
-          })
-        ]
-      })
-    );
-
-    workbox.precaching.precacheAndRoute(self.__precacheManifest);
-  } else {
-    console.error("Workbox could not be loaded. No offline support");
-  }
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
+} else {
+  console.error("Workbox could not be loaded. No offline support");
+}
